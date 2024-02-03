2d71e2a38ef636761f75902cbecf18de73693552
Sudhirkumarrao Allada <sudhir.kumarrao@aieze.in>
2024-02-04 00:41:46 +0530

    changes

--- utility/minioUtility.js
+++ utility/minioUtility.js
@@ -4,18 +4,18 @@ const util = require('util');
 // Initialize MinIO client with your server details
 const minioClient = new Minio.Client({
     endPoint: process.env.AIEZE_COMMON_MINIO_ENDPOINT,
-    port: process.env.AIEZE_COMMON_MINIO_PORT,
-    useSSL: process.env.AIEZE_COMMON_MINIO_USE_SSL,
+    port: process.env.AIEZE_COMMON_MINIO_PORT ? parseInt(process.env.AIEZE_COMMON_MINIO_PORT) : 9000,
+    useSSL: process.env.AIEZE_COMMON_MINIO_USE_SSL ? eval(process.env.AIEZE_COMMON_MINIO_USE_SSL) : false,
     accessKey: process.env.AIEZE_COMMON_MINIO_ACCESS_KEY,
     secretKey: process.env.AIEZE_COMMON_MINIO_SECRET_KEY,
 });
 
 
 const getObject = async(bucketName, objectName) => {
-    const getObjectAsync = util.promisify(minioClient.getObject.bind(minioClient));
+    // const getObjectAsync = util.promisify(minioClient.getObject.bind(minioClient));
     return new Promise(async(resolve, reject) => {
         try {
-            const dataStream = await getObjectAsync({ Bucket: bucketName, Object: objectName });
+            const dataStream = await minioClient.getObject(bucketName, objectName);
             
             const chunks = [];
             
@@ -39,11 +39,11 @@ const getObject = async(bucketName, objectName) => {
 
 
 const uploadObject = async(bucketName, objectName, dataBuffer) => {
-    const putObjectAsync = util.promisify(minioClient.putObject.bind(minioClient));
+    // const putObjectAsync = util.promisify(minioClient.putObject.bind(minioClient));
     return new Promise(async(resolve, reject) => {
         try {
-            await putObjectAsync(bucketName, objectName, dataBuffer, dataBuffer.length);
-            resolve()
+            const data = await minioClient.putObject(bucketName, objectName, dataBuffer);
+            resolve(data)
         } catch (err) {
             reject(err);
         }
